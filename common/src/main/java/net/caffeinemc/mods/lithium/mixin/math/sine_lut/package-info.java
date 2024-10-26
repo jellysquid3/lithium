128b223c8c37426eb50226d3a27c33f57c0b06cf
2No2Name <2no2name@web.de>
2024-10-26 17:02:52 +0200

    Disable math.sine_lut

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/math/sine_lut/package-info.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/math/sine_lut/package-info.java
@@ -1,4 +1,7 @@
-@MixinConfigOption(description = "Reduces the sine table size to reduce memory usage and increase access speed")
+@MixinConfigOption(
+        description = "Reduces the sine table size to reduce memory usage and increase access speed. Broken in 1.21.2 and 1.21.3.",
+        enabled = false
+)
 package net.caffeinemc.mods.lithium.mixin.math.sine_lut;
 
 import net.caffeinemc.gradle.MixinConfigOption;
