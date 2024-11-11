0d699d291a1ac89b829fec1919a77d6042f6bc1a
Space Walker <spacedoesrs@gmail.com>
2024-11-11 18:02:05 +0100

    update redstone wire optimizations
    also fixes #581

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/block/redstone_wire/DefaultRedstoneWireEvaluatorMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/block/redstone_wire/DefaultRedstoneWireEvaluatorMixin.java
@@ -3,35 +3,29 @@ package net.caffeinemc.mods.lithium.mixin.block.redstone_wire;
 import net.caffeinemc.mods.lithium.common.block.redstone.RedstoneWirePowerCalculations;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RedStoneWireBlock;
+import net.minecraft.world.level.redstone.DefaultRedstoneWireEvaluator;
 import net.minecraft.world.level.redstone.RedstoneWireEvaluator;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(RedStoneWireBlock.class)
-public class RedStoneWireBlockMixin extends Block {
+@Mixin(DefaultRedstoneWireEvaluator.class)
+public abstract class DefaultRedstoneWireEvaluatorMixin extends RedstoneWireEvaluator {
 
-    @Shadow
-    @Final
-    private RedstoneWireEvaluator evaluator;
-
-    public RedStoneWireBlockMixin(Properties settings) {
-        super(settings);
+    private DefaultRedstoneWireEvaluatorMixin(RedStoneWireBlock wireBlock) {
+        super(wireBlock);
     }
 
     @Inject(
-            method = "getBlockSignal(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)I",
+            method = "calculateTargetStrength(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)I",
             cancellable = true,
             at = @At(
                     value = "HEAD"
             )
     )
-    private void getBlockSignalFaster(Level world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(RedstoneWirePowerCalculations.getNeighborBlockSignal((Block) (Object) this, this.evaluator, world, pos));
+    private void calculateTargetStrengthFaster(Level level, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(RedstoneWirePowerCalculations.getNeighborSignal(this.wireBlock, this, level, pos));
     }
 }
--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/block/redstone_wire/RedstoneWireEvaluatorMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/block/redstone_wire/RedstoneWireEvaluatorMixin.java
@@ -3,7 +3,6 @@ package net.caffeinemc.mods.lithium.mixin.block.redstone_wire;
 import net.caffeinemc.mods.lithium.common.block.redstone.RedstoneWirePowerCalculations;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RedStoneWireBlock;
 import net.minecraft.world.level.redstone.RedstoneWireEvaluator;
 import org.spongepowered.asm.mixin.Final;
@@ -13,25 +12,21 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(RedStoneWireBlock.class)
-public class RedStoneWireBlockMixin extends Block {
+@Mixin(RedstoneWireEvaluator.class)
+public class RedstoneWireEvaluatorMixin {
 
     @Shadow
     @Final
-    private RedstoneWireEvaluator evaluator;
-
-    public RedStoneWireBlockMixin(Properties settings) {
-        super(settings);
-    }
+    private RedStoneWireBlock wireBlock;
 
     @Inject(
-            method = "getBlockSignal(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)I",
+            method = "getIncomingWireSignal(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)I",
             cancellable = true,
             at = @At(
                     value = "HEAD"
             )
     )
-    private void getBlockSignalFaster(Level world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(RedstoneWirePowerCalculations.getNeighborBlockSignal((Block) (Object) this, this.evaluator, world, pos));
+    private void getIncomingWireSignalFaster(Level level, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(RedstoneWirePowerCalculations.getNeighborWireSignal(this.wireBlock, (RedstoneWireEvaluator) (Object) this, level, pos));
     }
 }
