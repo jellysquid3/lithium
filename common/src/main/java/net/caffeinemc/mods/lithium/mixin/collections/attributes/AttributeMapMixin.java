463a55d5fcf98040b946e251fb9ef17040f1824b
2No2Name <2no2name@web.de>
2024-11-26 01:32:17 +0100

    Fix collections.attributes incorrectly using reference equality on record keys
    Relates to https://github.com/CaffeineMC/lithium-fabric/issues/587

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/collections/attributes/AttributeMapMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/collections/attributes/AttributeMapMixin.java
@@ -1,9 +1,6 @@
 package net.caffeinemc.mods.lithium.mixin.collections.attributes;
 
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
-import net.minecraft.core.Holder;
-import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -15,27 +12,27 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Map;
 import java.util.Set;
 
 @Mixin(AttributeMap.class)
 public class AttributeMapMixin {
-    @Mutable
-    @Shadow
-    @Final
-    private Map<Holder<Attribute>, AttributeInstance> attributes;
 
     @Mutable
     @Shadow
     @Final
     private Set<AttributeInstance> attributesToUpdate;
 
+    @Mutable
+    @Shadow
+    @Final
+    private Set<AttributeInstance> attributesToSync;
+
     @Inject(
             method = "<init>",
             at = @At("RETURN")
     )
     private void initCollections(AttributeSupplier defaultAttributes, CallbackInfo ci) {
-        this.attributes = new Reference2ReferenceOpenHashMap<>(0);
         this.attributesToUpdate = new ReferenceOpenHashSet<>(0);
+        this.attributesToSync = new ReferenceOpenHashSet<>(0);
     }
 }
