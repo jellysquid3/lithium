b5c0bbfed7873cdacc3f944cc25694c804890bed
2No2Name <2no2name@web.de>
2024-11-28 03:43:10 +0100

    Move POI collection optimization to fabric only package
    Fixes incompatibility with NeoForge POI Type registration logic
    https://github.com/CaffeineMC/lithium-fabric/issues/594

---
+++ fabric/src/main/java/net/caffeinemc/mods/lithium/fabric/mixin/collections/poi_types/package-info.java
@@ -0,0 +1,5 @@
+@MixinConfigOption(description = "Uses fastutil hashmap for POI Types")
+package net.caffeinemc.mods.lithium.fabric.mixin.collections.poi_types;
+
+import net.caffeinemc.gradle.MixinConfigOption;
+//Not on NeoForge, as it also replaces the map
