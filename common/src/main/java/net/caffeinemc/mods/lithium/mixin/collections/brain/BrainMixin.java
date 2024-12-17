44b156cd3724109f991fd9d163724747c79079a1
2No2Name <2no2name@web.de>
2024-12-17 02:36:17 +0100

    Use fast iterator in Brain outdated memory removal

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/collections/brain/BrainMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/collections/brain/BrainMixin.java
@@ -3,9 +3,10 @@ package net.caffeinemc.mods.lithium.mixin.collections.brain;
 import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceLinkedOpenHashMap;
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
 import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.ai.memory.ExpirableValue;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.entity.schedule.Activity;
@@ -15,11 +16,10 @@ import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Supplier;
 
 @Mixin(Brain.class)
@@ -28,7 +28,7 @@ public class BrainMixin {
     @Mutable
     @Shadow
     @Final
-    private Map<?, ?> memories;
+    private Map<MemoryModuleType<?>, Optional<? extends ExpirableValue<?>>> memories;
 
     @Mutable
     @Shadow
@@ -45,9 +45,25 @@ public class BrainMixin {
             at = @At("RETURN")
     )
     private void reinitializeBrainCollections(Collection<?> memories, Collection<?> sensors, ImmutableList<?> memoryEntries, Supplier<?> codecSupplier, CallbackInfo ci) {
-        this.memories = new Reference2ReferenceOpenHashMap<>(this.memories);
+        this.memories = new Reference2ObjectOpenHashMap<>(this.memories);
         this.sensors = new Reference2ReferenceLinkedOpenHashMap<>(this.sensors);
         this.activityRequirements = new Object2ObjectOpenHashMap<>(this.activityRequirements);
+    }
+
+    @Redirect(
+            method = "forgetOutdatedMemories", at = @At(value = "INVOKE", target = "Ljava/util/Map;entrySet()Ljava/util/Set;")
+    )
+    private <K,V> Set<Map.Entry<K, V>> redirectIterator(Map<K, V> instance) {
+        return null;
+    }
+    @Redirect(
+            method = "forgetOutdatedMemories", at = @At(value = "INVOKE", target = "Ljava/util/Set;iterator()Ljava/util/Iterator;")
+    )
+    private Iterator<? extends Map.Entry<?, ?>> redirectIterator(Set<Map.Entry<MemoryModuleType<?>, Optional<? extends ExpirableValue<?>>>> set) {
+        if (this.memories instanceof Reference2ObjectOpenHashMap<MemoryModuleType<?>, Optional<? extends ExpirableValue<?>>> fastMap) {
+            return fastMap.reference2ObjectEntrySet().fastIterator();
+        }
+        return this.memories.entrySet().iterator();
     }
 
 }
