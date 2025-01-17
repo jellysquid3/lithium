d7288bf78bbf91d60b74e33da5a30913430b3386
2No2Name <2No2Name@web.de>
2025-01-17 14:35:14 +0100

    Disable LithiumHashPalette optimization due to crash issues

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/chunk/palette/package-info.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/chunk/palette/package-info.java
@@ -1,4 +1,7 @@
-@MixinConfigOption(description = "Replaces the vanilla hash palette with an optimized variant")
+@MixinConfigOption(
+        description = "Replaces the vanilla hash palette with an optimized variant. Disabled by default as many users report crashes regarding or caused by this setting.",
+        enabled = false
+)
 package net.caffeinemc.mods.lithium.mixin.chunk.palette;
 
 import net.caffeinemc.gradle.MixinConfigOption;
